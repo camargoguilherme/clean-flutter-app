6e0fb2d06f6e72ac611bbc0c51181a59f80e2e19
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2024-01-19 02:05:58 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,10 +2,11 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:ForDev/domain/helpers/helpers.dart';
 import 'package:ForDev/domain/usecases/authentication.dart';
 
-import 'package:ForDev/data/usecases/usecases.dart';
 import 'package:ForDev/data/http/http.dart';
+import 'package:ForDev/data/usecases/usecases.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -31,5 +32,21 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.secret},
     ));
+  });
+
+  test('Should throw UnexoectedError if HttpClient returns 400', () async {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    final furure = sut.auth(params);
+
+    expect(furure, throwsA(DomainError.unexpected));
   });
 }
